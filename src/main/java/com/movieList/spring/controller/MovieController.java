9d09a1098274d412cae118f20accd14bef31bb60
IslamGoher <islam.m.goher@gmail.com>
2022-04-29 07:49:50 +0200

    Rewrite logic inside service
    - Write the logic inside service not controller

--- src/main/java/com/movieList/spring/controller/MovieController.java
+++ src/main/java/com/movieList/spring/controller/MovieController.java
@@ -1,16 +1,11 @@
 package com.movieList.spring.controller;
 
-import com.movieList.spring.errorHandling.ErrorHandler;
-import com.movieList.spring.errorHandling.ErrorResponse;
-import com.movieList.spring.models.Movie;
 import com.movieList.spring.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1")
@@ -23,31 +18,12 @@ public class MovieController {
   }
 
 
-
   // @route   GET '/api/v1/movies'
   // @desc    list all movies
   // @access  public
   @GetMapping(path = "/movies")
   public Object getMovies(HttpServletResponse res) {
-    List<Movie> movies = null;
-
-    try {
-      // get movies from database
-      movies = movieService.getMovies();
-
-      // check if movies exists
-      if(movies.isEmpty()) {
-        res.setStatus(404);
-        return new ErrorResponse(404, "there's no such movies found");
-      }
-
-    } catch (Exception err) {
-      return ErrorHandler.handle(err, res);
-    }
-
-    // send response
-    res.setStatus(200);
-    return movies;
+    return movieService.getMovies(res);
   }
 
 }
--- src/main/java/com/movieList/spring/service/MovieService.java
+++ src/main/java/com/movieList/spring/service/MovieService.java
@@ -1,10 +1,13 @@
 package com.movieList.spring.service;
 
+import com.movieList.spring.errorHandling.ErrorHandler;
+import com.movieList.spring.errorHandling.ErrorResponse;
 import com.movieList.spring.models.Movie;
 import com.movieList.spring.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Service
@@ -17,7 +20,24 @@ public class MovieService {
   }
 
   // @route   GET '/api/v1/movies'
-  public List<Movie> getMovies() {
-    return movieRepository.getAllMovies();
+  // @desc    list all movies
+  // @access  public
+  public Object getMovies(HttpServletResponse res) {
+    List<Movie> movies = null;
+    try {
+      // get movies from database
+      movies = movieRepository.getAllMovies();
+
+      // check if movies exists
+      if(movies.isEmpty()) {
+        res.setStatus(404);
+        return new ErrorResponse(404, "there's no such movies found");
+      }
+
+    } catch (Exception err) {
+      return ErrorHandler.handle(err, res);
+    }
+
+    return movies;
   }
 }
